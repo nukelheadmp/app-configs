e9bd6e0c52fb3ddcc576b06b244ac58be5680e26
Britt Carter <bcarter@priefert.com>
2026-07-13 15:45:25 -0500

    Added function for running ansible on localhost

--- ansible/bashrc.d/99-ansible.sh
+++ ansible/bashrc.d/99-ansible.sh
@@ -1,3 +1,30 @@
+ansible-local() {
+  if [[ -z "$1" ]]; then
+    echo -e "Usage: ansible-local <playbook> [options...]"
+    return 1
+  fi
+
+  if [[ "$PWD" != "${PROJECTSDIR}/ansible-servers" ]]; then
+    echo -e " -> Moving to ansible directory..."
+    cd ${PROJECTSDIR}/ansible-servers
+  fi
+
+  local playbook="playbooks/${1}"
+
+  if [[ ! -f "${playbook}" ]]; then
+    echo -e "Error: Playbook not found: ${playbook}"
+    return 1
+  fi
+
+  if [[ -z "${VIRTUAL_ENV:-}" || "${VIRTUAL_ENV}" != "${PYENV_PATH}/ansible" ]]; then
+    activate ansible
+  fi
+
+  ansible-playbook "$playbook" "${@:2}" \
+    -i "localhost," -c local \
+    --ask-become-pass
+}
+
 _ansible-passbolt() {
   local directory="$1"
   shift
