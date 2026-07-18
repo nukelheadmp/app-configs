dbf163878f2a1866140752af5f57dff0e3adea5e
Britt Carter <nukelheadmp@gmail.com>
2026-07-18 17:42:03 -0500

    Added git config script, auto creation of python venv, and corrected app-configs path

--- ansible/ansible.sh
+++ ansible/ansible.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-echo "Install Ansible and set up environment for Passbolt integration"
+echo "Install Ansible with Passbolt integration and custom functions"
 
 echo "Checking environment variables"
 if [[ -z ${PROJECTSDIR:-} ]]; then
-  echo "Setting necessary variables"
+  echo "Setting required variables"
   export PROJECTSDIR=$HOME/Projects
   export PYENV_PATH=$HOME/.local/lib/python
   export ANSIBLE_VAULTS=$HOME/.ansible/vaults
   echo "Copying environment config file"
   mkdir -p $HOME/.config/environment.d
-  cp $PROJECTSDIR/installation-scripts/env/projects.sh $HOME/.config/environment.d/
+  cp $PROJECTSDIR/app-configs/env/projects.conf $HOME/.config/environment.d/
 fi
+
+echo "Add bash functions"
+if [[ ! -d $HOME/.bashrc.d ]]; then
+  mkdir -p $HOME/.bashrc.d
+fi
+
+cp $PROJECTSDIR/app-configs/ansible/bashrc.d/* $HOME/.bashrc.d
+source $HOME/.bashrc
 
 echo "Installing packages"
 sudo dnf install -y \
@@ -26,7 +34,7 @@ cd $PROJECTSDIR/passbolt-ansible-lookup-plugin
 
 echo "Set up python environment"
 python3 -m venv $PYENV_PATH/ansible
-source $PYENV_PATH/ansible/bin/activate
+activate ansible
 pip install -r passbolt/passbolt_lookup/requirements.txt
 pip install pywinrm ncclient jxmlease xmltodict
 
@@ -35,14 +43,7 @@ ansible-galaxy collection install ./passbolt --force
 
 echo "Copy Ansible/Passbolt config file"
 mkdir -p $ANSIBLE_VAULTS
-cp $PROJECTSDIR/installation-scripts/ansible/vault_passbolt.yml $ANSIBLE_VAULTS/vault_passbolt.yml
-
-if [[ ! -d $HOME/.bashrc.d ]]; then
-  mkdir -p $HOME/.bashrc.d
-fi
-
-cp $PROJECTSDIR/installation-scripts/ansible/bashrc.d/* $HOME/.bashrc.d
-source $HOME/.bashrc
+cp $PROJECTSDIR/app-configs/ansible/vault_passbolt.yml $ANSIBLE_VAULTS/vault_passbolt.yml
 
 $EDITOR $ANSIBLE_VAULTS/vault_passbolt.yml
 
--- ansible/bashrc.d/99-python.sh
+++ ansible/bashrc.d/99-python.sh
@@ -3,8 +3,17 @@ activate() {
     echo "You must provide the name of a Python virtual environment"
     return
   elif [[ ! -d "${PYENV_PATH}/${@}" ]]; then
+
     echo "Virtual environment \"${@}\" does not exist."
+    read -p "Would you like to create it? (y/n): " response
+
+    if [[ -n $response && ($response == "y" || $response == "Y") ]]; then
+      python3 -m venv $PYENV_PATH/${@}
+      source ${PYENV_PATH}/${@}/bin/activate
+    fi
+
     return
+
   else
     source ${PYENV_PATH}/${@}/bin/activate
   fi
