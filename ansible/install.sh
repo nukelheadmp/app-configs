740df0728e0e2db1e91e2741e07f5514b6a73b73
Britt Carter <nukelheadmp@gmail.com>
2026-07-19 00:54:27 -0500

    Restructured and modified to operate from users .local

--- ansible/install.sh
+++ ansible/install.sh
@@ -3,14 +3,12 @@
 echo "Install Ansible with Passbolt integration and custom functions"
 
 echo "Checking environment variables"
-if [[ -z ${PROJECTSDIR:-} ]]; then
+if [[ -z ${ANSIBLE_VAULTS:-} ]]; then
   echo "Setting required variables"
-  export PROJECTSDIR=$HOME/Projects
-  export PYENV_PATH=$HOME/.local/lib/python
   export ANSIBLE_VAULTS=$HOME/.ansible/vaults
-  echo "Copying environment config file"
-  mkdir -p $HOME/.config/environment.d
-  cp $PROJECTSDIR/app-configs/env/projects.conf $HOME/.config/environment.d/
+  export PYENV_PATH=$HOME/.local/lib/python
+  echo "Install environment conf files"
+  cp $HOME/.local/share/app-configs/ansible/environment.d/* $HOME/.config/environment.d/
 fi
 
 echo "Add bash functions"
@@ -18,7 +16,7 @@ if [[ ! -d $HOME/.bashrc.d ]]; then
   mkdir -p $HOME/.bashrc.d
 fi
 
-cp $PROJECTSDIR/app-configs/ansible/bashrc.d/* $HOME/.bashrc.d
+cp $HOME/.local/share/app-configs/ansible/bashrc.d/* $HOME/.bashrc.d/
 source $HOME/.bashrc
 
 echo "Installing packages"
@@ -28,9 +26,8 @@ sudo dnf install -y \
   sshpass
 
 echo "Clone Passbolt plugin repo"
-mkdir -p $PROJECTSDIR
-git clone https://github.com/passbolt/passbolt-ansible-lookup-plugin.git $PROJECTSDIR/passbolt-ansible-lookup-plugin
-cd $PROJECTSDIR/passbolt-ansible-lookup-plugin
+git clone https://github.com/passbolt/passbolt-ansible-lookup-plugin.git $HOME/.local/share/passbolt-ansible-lookup-plugin
+cd $HOME/.local/share/passbolt-ansible-lookup-plugin
 
 echo "Set up python environment"
 python3 -m venv $PYENV_PATH/ansible
@@ -43,9 +40,15 @@ ansible-galaxy collection install ./passbolt --force
 
 echo "Copy Ansible/Passbolt config file"
 mkdir -p $ANSIBLE_VAULTS
-cp $PROJECTSDIR/app-configs/ansible/vault_passbolt.yml $ANSIBLE_VAULTS/vault_passbolt.yml
+cp $HOME/.local/share/app-configs/ansible/vault_passbolt.yml $ANSIBLE_VAULTS/vault_passbolt.yml
 
 $EDITOR $ANSIBLE_VAULTS/vault_passbolt.yml
 
 echo "Install Juniper Collection"
 ansible-galaxy collection install juniper.device
+
+echo "A reboot, or logging out and back in recommended."
+read -p "Reboot now? (y/n)" reboot_confirm
+if [[ -n $reboot_confirm && ($reboot_confirm == "y" || $reboot_confirm == "Y") ]]; then
+  reboot
+fi
