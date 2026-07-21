c68ba515a69d2f8d2f07b616e10c1ea9459c20c0
Britt Carter <bcarter@priefert.com>
2026-07-21 16:01:57 -0500

    Added option to update existing settings

--- git_config.sh
+++ git_config.sh
@@ -2,8 +2,15 @@
 
 user_name=$(git config user.name 2>/dev/null)
 user_email=$(git config user.email 2>/dev/null)
+force_change="n"
 
-if [[ -z "${user_name:-}" || -z "${user_email}" ]]; then
+if [[ -n "${user_name:-}" ]]; then
+  echo "Current settings:"
+  echo "Name: $user_name"
+  echo "Email: $user_email"
+  read -p "Do you want to change settings? [y/n]: " force_change
+
+  if [[ -z "${user_name:-}" || -z "${user_email}" || ( $force_change == "y" || $force_change == "Y" ) ]]; then
   read -p "Enter your full name: " user_name
   read -p "Enter your email: " user_email
   git config --global user.name "$user_name"
