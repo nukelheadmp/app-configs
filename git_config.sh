1465dbd99222ece85cb7750d74389478c253f5f1
Britt Carter <bcarter@priefert.com>
2026-07-21 11:22:09 -0500

    Check for name OR email, always config everything else

--- git_config.sh
+++ git_config.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 user_name=$(git config user.name 2>/dev/null)
+user_email=$(git config user.email 2>/dev/null)
 
-if [[ -z "${user_name:-}" ]]; then
+if [[ -z "${user_name:-}" || -z "${user_email}" ]]; then
   read -p "Enter your full name: " user_name
   read -p "Enter your email: " user_email
   git config --global user.name "$user_name"
   git config --global user.email "$user_email"
-  git config --global pull.rebase false
-  git config --global init.defaultBranch master
-  git config --global push.autoSetupRemote true
 fi
+
+git config --global pull.rebase false
+git config --global init.defaultBranch master
+git config --global push.autoSetupRemote true
