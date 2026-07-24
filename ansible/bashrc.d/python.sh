839bf3aa1470415de8a285a3fc2f250df9f2f1ce
Britt Carter <bcarter@priefert.com>
2026-07-24 14:11:24 -0500

    Changed brackets to be consistent with other prompts

--- ansible/bashrc.d/python.sh
+++ ansible/bashrc.d/python.sh
@@ -5,7 +5,7 @@ activate() {
   elif [[ ! -d "${PYENV_PATH}/${@}" ]]; then
 
     echo "Virtual environment \"${@}\" does not exist."
-    read -p "Would you like to create it? (y/n): " response
+    read -p "Would you like to create it? [y/n]: " response
 
     if [[ -n $response && ($response == "y" || $response == "Y") ]]; then
       python3 -m venv $PYENV_PATH/${@}
