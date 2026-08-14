2f5d034d2a6b0d36014473ded5c60737bd5170d1
Britt Carter <bcarter@priefert.com>
2026-08-14 11:55:33 -0500

    Added CA certs to trust

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+sudo dnf install ca-certificates
+sudo cp ~/.local/share/app-configs/certs/* /etc/pki/ca-trust/source/anchors/
+sudo update-ca-trust extract
+
 echo "Configure git"
 $HOME/.local/share/app-configs/git_config.sh
 
