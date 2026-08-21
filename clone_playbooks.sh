1e2924a7209f76441bd4a07fef63169fd1dc3b21
Britt Carter <nukelheadmp@gmail.com>
2026-08-21 00:26:49 -0500

    Removed sudo from playbook cloning

--- clone_playbooks.sh
+++ clone_playbooks.sh
@@ -2,13 +2,13 @@
 
 echo "Clone Ansible playbook repos"
 if [[ ! -d ${HOME}/Projects/ansible-network/ ]]; then
-  sudo git clone git@gitlab.priefert.com:infotech/ansible-network.git ${HOME}/Projects/ansible-network
+  git clone git@gitlab.priefert.com:infotech/ansible-network.git ${HOME}/Projects/ansible-network
 else
   echo "Directory ${HOME}/Projects/ansible-network/ already exists."
 fi
 
 if [[ ! -d ${HOME}/Projects/ansible-servers/ ]]; then
-  sudo git clone git@gitlab.priefert.com:infotech/ansible-servers.git ${HOME}/Projects/ansible-servers
+  git clone git@gitlab.priefert.com:infotech/ansible-servers.git ${HOME}/Projects/ansible-servers
 else
   echo "Directory ${HOME}/Projects/ansible-servers/ already exists."
 fi
