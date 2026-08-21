96477650e514eab9cdb6b923ea235b94bd2dc7f8
Britt Carter <nukelheadmp@gmail.com>
2026-08-20 21:53:37 -0500

    Set origin url back to ssh

--- clone_playbooks.sh
+++ clone_playbooks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Clone Ansible playbook repos"
-sudo git clone https://gitlab.priefert.com/infotech/ansible-network.git ${HOME}/Projects/ansible-network
-sudo git clone https://gitlab.priefert.com/infotech/ansible-servers.git ${HOME}/Projects/ansible-servers
+sudo git clone git@gitlab.priefert.com:infotech/ansible-network.git ${HOME}/Projects/ansible-network
+sudo git clone git@gitlab.priefert.com:infotech/ansible-servers.git ${HOME}/Projects/ansible-servers
