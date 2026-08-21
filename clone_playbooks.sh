947070101fa8c34af353258a66962ef00dedc28b
Britt Carter <nukelheadmp@gmail.com>
2026-08-20 21:59:10 -0500

    Added check for existing playbook directories

--- clone_playbooks.sh
+++ clone_playbooks.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
 echo "Clone Ansible playbook repos"
-sudo git clone git@gitlab.priefert.com:infotech/ansible-network.git ${HOME}/Projects/ansible-network
-sudo git clone git@gitlab.priefert.com:infotech/ansible-servers.git ${HOME}/Projects/ansible-servers
+if [[ ! -d ${HOME}/Projects/ansible-network/ ]]; then
+  sudo git clone git@gitlab.priefert.com:infotech/ansible-network.git ${HOME}/Projects/ansible-network
+else
+  echo "Directory ${HOME}/Projects/ansible-network/ already exists."
+fi
+
+if [[ ! -d ${HOME}/Projects/ansible-servers/ ]]; then
+  sudo git clone git@gitlab.priefert.com:infotech/ansible-servers.git ${HOME}/Projects/ansible-servers
+else
+  echo "Directory ${HOME}/Projects/ansible-servers/ already exists."
+fi
